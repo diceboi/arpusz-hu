69733c35cb9ce3e90a5a6ec036ea4a8286a9a882
diceboi <122260041+diceboi@users.noreply.github.com>
2023-05-10 02:23:17 +0200

    fixed document import

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,13 @@ import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
 
 import MainMenu from '@/components/mainmenu'
-import { Main } from 'next/document'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function MyApp({ Component, pageProps, className }) {
   return (
     <main className={inter.className}>
-      <MainMenu className={className} />
+      <MainMenu />
       <Component {...pageProps}>
       </Component >
     </main>
