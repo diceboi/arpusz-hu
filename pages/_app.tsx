7d2ad2cf03d02d0950eb69871e2b279198a50330
diceboi <122260041+diceboi@users.noreply.github.com>
2023-06-04 02:46:32 +0200

    próba tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,23 @@
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 import MainMenu from '@/components/mainmenu'
 import Footer from '@/components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function MyApp({ Component, pageProps, className }) {
+export default function MyApp({ Component, pageProps, }: AppProps) {
   return (
+    <>
     <main className={inter.className}>
       <MainMenu />
-      <Component {...pageProps} className="w-screen overflow-hidden">
+      <Component {...pageProps} >
       </Component >
+      <Analytics />
       <Footer />
     </main>
+    </>
   )
 }
