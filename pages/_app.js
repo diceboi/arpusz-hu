6d29eac7952a79e4c47be767373a1a9ed7a6f64b
diceboi <122260041+diceboi@users.noreply.github.com>
2023-06-19 22:15:10 +0200

    hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -26,6 +26,22 @@ export default function MyApp({ Component, pageProps, className }) {
         `,
         }} 
       />
+      <Script 
+        id='hotjar'
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3258159,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `
+        }}
+      
+      />
     <MainMenu />    
     <main className={inter.className}>
       <Component {...pageProps} className="w-screen overflow-hidden">
